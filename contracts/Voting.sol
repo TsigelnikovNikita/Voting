9b45e894fcc6c60a420b504edb3e7792eaa55317
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-05-18 02:10:31 +0300

    added basic structs and vars

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,6 +2,30 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Voting {
+contract Voting is Ownable {
+    uint32 constant public VOTE_DURATION = 3 days;
+
+    // fee of every vote that owner 
+    uint8 constact public FEE = 10; // 10%
+
+    // amount of vee that owner can withdraw
+    uint availableFee;
+
+    struct Candidate {
+        address addr;
+        uint32 amountOfVote;
+    }
+
+    struct Vote {
+        Candidate[] candidates;
+        string name;
+        uint pool;
+        uint64 endOfVote;
+        bool isEnded;
+        mapping (address => bool) alreadyVoted;
+    }
+
+    Vote[] public votes;
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+require('hardhat-dependency-compiler');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  dependencyCompiler: {
+    paths: [
+      '@openzeppelin/contracts/access/Ownable.sol',
+    ],
+  }
 };
