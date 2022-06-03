341242b177c5a4eab626aa658cf8396bf8a1347f
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-06-03 14:11:19 +0300

    added optimizer flag

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,15 @@ require('solidity-coverage');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }
+  },
   dependencyCompiler: {
     paths: [
       '@openzeppelin/contracts/access/Ownable.sol',
